9b48f5fc67387ca4c968d41e180398cec1145414
Ed Simmons <e.d.simmons@outlook.com>
2023-05-14 11:51:55 +0100

    Add fontawesome

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,15 @@ import router from './router'
 
 import './assets/main.css'
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faUserSecret)
+
 const app = createApp(App)
 
+app.component('font-awesome-icon', FontAwesomeIcon)
 app.use(router)
 
 app.mount('#app')
